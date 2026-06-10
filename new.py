d3d5fbb700ceb31b612bd3016ff3e4fb8e398fb4
PRIYA MISHRA <freedompiya1880@gmail.com>
2026-06-10 07:20:16 +0000

    add flask registration flask

---
+++ new.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import os
+
+# INTERACTION
+app = Flask(__name__)
+
+picfolder = os.path.join('static')
+
+app.config['UPLOAD_FOLDER'] = picfolder
+
+# MAPPING
+@app.route('/')
+
+# INPUTS
+def first():
+    pic = os.path.join(app.config['UPLOAD_FOLDER'], 'pic.jpg')
+    return render_template('home.html', user_image=pic)
+
+# MAPPING
+@app.route('/second')
+
+# INPUTS
+def second():
+    return "WELCOME TO SECOND PAGE"
+# MAIN
+if __name__ == "__main__":
+    app.run(debug=True)
